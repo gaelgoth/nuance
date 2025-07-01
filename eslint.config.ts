0b5a6ff144e5db1a112157e36fc8c21a02cfd8da
Gaël G. <55390114+gaelgoth@users.noreply.github.com>
2025-07-01 20:16:27 +0200

    Initial commit
    Signed-off-by: gaelgoth <gothuey.gael@gmail.com>

---
+++ eslint.config.ts
@@ -0,0 +1,8 @@
+// eslint.config.ts
+import { FlatConfigItem } from 'eslint';
+
+const config: FlatConfigItem[] = [
+  // Add your ESLint config here
+];
+
+export default config;
---
+++ src/assets/logos.ts
@@ -0,0 +1,4 @@
+import viteLogo from '/vite.svg';
+import reactLogo from './assets/react.svg';
+
+export { viteLogo, reactLogo };
